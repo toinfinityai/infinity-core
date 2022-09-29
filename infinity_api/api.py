4c77e3e2a37366c1b2ff81669ae0c2083562fd00
Daniel Hensley <hensley.daniel@gmail.com>
2022-09-28 23:50:14 -0500

    Clean up and polish `api` module GET functions.

--- infinity_api/api.py
+++ infinity_api/api.py
@@ -2,7 +2,7 @@ from functools import reduce
 from operator import ior
 from urllib.parse import urlencode, urljoin
 from datetime import datetime, timedelta
-from typing import Any, Optional, Dict, Tuple, Set
+from typing import Any, Optional, Dict, Tuple, Set, List, Union
 
 import requests
 
@@ -24,81 +24,93 @@ def build_get_request(
     if query_parameters:
         url += "?" + urlencode(query_parameters)
         url = _ensure_trailing_slash(url)
-    headers = reduce(ior, [h.to_header_dict(token) for h in headers], dict())
+    headers_dict: Dict[str, str] = reduce(ior, [h.to_header_dict(token) for h in headers], dict())
 
-    return url, headers
+    return url, headers_dict
 
 
-def get_all_preview_data(token: str, server: str = DEFAULT_SERVER):
+def get_all_preview_data(token: str, server: str = DEFAULT_SERVER) -> List[Dict[str, Any]]:
     url, headers = build_get_request(
         token=token,
         server=server,
         endpoint="api/job_previews/",
         headers=set([HeaderKind.AUTH, HeaderKind.ACCEPT_JSON]),
     )
-    print(url)
-    print(headers)
-    return requests.get(url=url, headers=headers)
+    r = requests.get(url=url, headers=headers)
+    r.raise_for_status()
+    return r.json()
 
 
-def get_single_preview_data(token: str, preview_id: str, server: str = DEFAULT_SERVER):
+def get_single_preview_data(token: str, preview_id: str, server: str = DEFAULT_SERVER) -> Dict[str, Any]:
     url, headers = build_get_request(
         token=token,
         server=server,
         endpoint=f"api/job_previews/{preview_id}/",
         headers=set([HeaderKind.AUTH, HeaderKind.ACCEPT_JSON]),
     )
-    return requests.get(url=url, headers=headers)
+    r = requests.get(url=url, headers=headers)
+    r.raise_for_status()
+    return r.json()
 
 
-def get_all_standard_job_data(token: str, server: str = DEFAULT_SERVER):
+def get_all_standard_job_data(token: str, server: str = DEFAULT_SERVER) -> List[Dict[str, Any]]:
     url, headers = build_get_request(
         token=token,
         server=server,
-        endpoint=f"api/job_runs/",
+        endpoint="api/job_runs/",
         headers=set([HeaderKind.AUTH, HeaderKind.ACCEPT_JSON]),
     )
-    return requests.get(url=url, headers=headers)
+    r = requests.get(url=url, headers=headers)
+    r.raise_for_status()
+    return r.json()
 
 
-def get_single_standard_job_data(token: str, standard_job_id: str, server: str = DEFAULT_SERVER):
+def get_single_standard_job_data(token: str, standard_job_id: str, server: str = DEFAULT_SERVER) -> Dict[str, Any]:
     url, headers = build_get_request(
         token=token,
         server=server,
         endpoint=f"api/job_runs/{standard_job_id}/",
         headers=set([HeaderKind.AUTH, HeaderKind.ACCEPT_JSON]),
     )
-    return requests.get(url=url, headers=headers)
+    r = requests.get(url=url, headers=headers)
+    r.raise_for_status()
+    return r.json()
 
 
-def get_all_generator_data(token: str, server: str = DEFAULT_SERVER):
+def get_all_generator_data(token: str, server: str = DEFAULT_SERVER) -> List[Dict[str, Any]]:
     url, headers = build_get_request(
         token=token,
         server=server,
-        endpoint=f"api/jobs/",
+        endpoint="api/jobs/",
         headers=set([HeaderKind.AUTH, HeaderKind.ACCEPT_JSON]),
     )
-    return requests.get(url=url, headers=headers)
+    r = requests.get(url=url, headers=headers)
+    r.raise_for_status()
+    return r.json()
 
 
-def get_single_generator_data(token: str, generator_name: str, server: str = DEFAULT_SERVER):
+def get_single_generator_data(token: str, generator_name: str, server: str = DEFAULT_SERVER) -> Dict[str, Any]:
     url, headers = build_get_request(
         token=token,
         server=server,
         endpoint=f"api/jobs/{generator_name}/",
         headers=set([HeaderKind.AUTH, HeaderKind.ACCEPT_JSON]),
     )
-    return requests.get(url=url, headers=headers)
+    r = requests.get(url=url, headers=headers)
+    r.raise_for_status()
+    return r.json()
 
 
-def get_openapi_schema(token: str, format: str = "yaml", language: str = "en", server: str = DEFAULT_SERVER):
-    headers = {HeaderKind.AUTH}
+def get_openapi_schema(
+    token: str, format: str = "yaml", language: str = "en", server: str = DEFAULT_SERVER
+) -> Union[Dict[str, Any], str]:
+    headers_set = {HeaderKind.AUTH}
     query_parameters = dict()
     if format == "yaml":
-        headers.add(HeaderKind.ACCEPT_OPENAPI_YAML)
+        headers_set.add(HeaderKind.ACCEPT_OPENAPI_YAML)
         query_parameters["format"] = format
     elif format == "json":
-        headers.add(HeaderKind.ACCEPT_OPENAPI_JSON)
+        headers_set.add(HeaderKind.ACCEPT_OPENAPI_JSON)
         query_parameters["format"] = format
     else:
         raise ValueError(f"Unsupported OpenAPI schema format: `{format}` must be `yaml` or `json`")
@@ -106,11 +118,16 @@ def get_openapi_schema(token: str, format: str = "yaml", language: str = "en", s
     url, headers = build_get_request(
         token=token,
         server=server,
-        endpoint=f"api/schema/",
-        headers=headers,
+        endpoint="api/schema/",
+        headers=headers_set,
         query_parameters=query_parameters,
     )
-    return requests.get(url=url, headers=headers)
+    r = requests.get(url=url, headers=headers)
+    r.raise_for_status()
+    if format == "yaml":
+        return r.text
+    else:
+        return r.json()
 
 
 def get_usage_datetime_range(
@@ -119,32 +136,28 @@ def get_usage_datetime_range(
     end_time: Optional[datetime] = None,
     server: str = DEFAULT_SERVER,
 ) -> Dict[str, Any]:
-
     if end_time is not None and start_time is not None:
         if end_time < start_time:
             raise ValueError(f"End time ({end_time}) before start time ({start_time}) for usage query")
-
-    query_dict = dict()
+    query_parameters = dict()
     if start_time is not None:
         if start_time.tzinfo is None:
             start_time = start_time.astimezone()
-        query_dict["start_time"] = start_time.isoformat()
+        query_parameters["start_time"] = start_time.isoformat()
     if end_time is not None:
         if end_time.tzinfo is None:
             end_time = end_time.astimezone()
-        query_dict["end_time"] = end_time.isoformat()
-
-    query_url = server + "/api/job_runs/counts/" + "?" + urlencode(query_dict)
-
-    r = requests.get(
-        query_url,
-        headers={"Authorization": f"Token {token}"},
+        query_parameters["end_time"] = end_time.isoformat()
+    url, headers = build_get_request(
+        token=token,
+        server=server,
+        endpoint="api/job_runs/counts/",
+        headers=set([HeaderKind.AUTH, HeaderKind.ACCEPT_JSON]),
+        query_parameters=query_parameters,
     )
-
-    if r.status_code == 200:
-        return r.json()
-    else:
-        raise ValueError(f"Error querying usage stats (status code {r.status_code}), details: {r.json()['detail']}")
+    r = requests.get(url=url, headers=headers)
+    r.raise_for_status()
+    return r.json()
 
 
 def get_usage_last_n_days(
@@ -161,5 +174,5 @@ def post_preview():
     pass
 
 
-def post_job():
+def post_standard_job():
     pass
